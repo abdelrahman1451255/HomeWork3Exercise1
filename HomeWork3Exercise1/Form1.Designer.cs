8ed81ab84b36c69632e9144e2f9fa37195d9dac4
abdelrahman1451255 <128540318+abdelrahman1451255@users.noreply.github.com>
2023-04-05 06:44:50 +0300

    Add files via upload

---
+++ HomeWork3Exercise1/Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace HomeWork3Exercise1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Title = new System.Windows.Forms.Label();
+            this.FirstName = new System.Windows.Forms.Label();
+            this.LastName = new System.Windows.Forms.Label();
+            this.FullName = new System.Windows.Forms.Label();
+            this.textBox1Title = new System.Windows.Forms.TextBox();
+            this.textBoxFirstName = new System.Windows.Forms.TextBox();
+            this.textBoxLastName = new System.Windows.Forms.TextBox();
+            this.textBoxFullName = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Title
+            // 
+            this.Title.AutoSize = true;
+            this.Title.Location = new System.Drawing.Point(42, 27);
+            this.Title.Name = "Title";
+            this.Title.Size = new System.Drawing.Size(30, 13);
+            this.Title.TabIndex = 0;
+            this.Title.Text = "Title:";
+            this.Title.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // FirstName
+            // 
+            this.FirstName.AutoSize = true;
+            this.FirstName.Location = new System.Drawing.Point(12, 74);
+            this.FirstName.Name = "FirstName";
+            this.FirstName.Size = new System.Drawing.Size(60, 13);
+            this.FirstName.TabIndex = 1;
+            this.FirstName.Text = "First Name:";
+            this.FirstName.Click += new System.EventHandler(this.FirstName_Click);
+            // 
+            // LastName
+            // 
+            this.LastName.AutoSize = true;
+            this.LastName.Location = new System.Drawing.Point(11, 104);
+            this.LastName.Name = "LastName";
+            this.LastName.Size = new System.Drawing.Size(61, 13);
+            this.LastName.TabIndex = 2;
+            this.LastName.Text = "Last Name:";
+            // 
+            // FullName
+            // 
+            this.FullName.AutoSize = true;
+            this.FullName.Location = new System.Drawing.Point(15, 137);
+            this.FullName.Name = "FullName";
+            this.FullName.Size = new System.Drawing.Size(57, 13);
+            this.FullName.TabIndex = 3;
+            this.FullName.Text = "Full Name:";
+            // 
+            // textBox1Title
+            // 
+            this.textBox1Title.Location = new System.Drawing.Point(114, 27);
+            this.textBox1Title.Name = "textBox1Title";
+            this.textBox1Title.Size = new System.Drawing.Size(123, 20);
+            this.textBox1Title.TabIndex = 4;
+            // 
+            // textBoxFirstName
+            // 
+            this.textBoxFirstName.Location = new System.Drawing.Point(114, 74);
+            this.textBoxFirstName.Name = "textBoxFirstName";
+            this.textBoxFirstName.Size = new System.Drawing.Size(123, 20);
+            this.textBoxFirstName.TabIndex = 5;
+            this.textBoxFirstName.TextChanged += new System.EventHandler(this.textBoxFirstName_TextChanged);
+            // 
+            // textBoxLastName
+            // 
+            this.textBoxLastName.Location = new System.Drawing.Point(114, 101);
+            this.textBoxLastName.Name = "textBoxLastName";
+            this.textBoxLastName.Size = new System.Drawing.Size(123, 20);
+            this.textBoxLastName.TabIndex = 6;
+            this.textBoxLastName.TextChanged += new System.EventHandler(this.textBoxLastName_TextChanged);
+            // 
+            // textBoxFullName
+            // 
+            this.textBoxFullName.Location = new System.Drawing.Point(114, 137);
+            this.textBoxFullName.Name = "textBoxFullName";
+            this.textBoxFullName.ReadOnly = true;
+            this.textBoxFullName.Size = new System.Drawing.Size(123, 20);
+            this.textBoxFullName.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Control;
+            this.button1.Location = new System.Drawing.Point(332, 27);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(137, 27);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "change Title";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button2.Location = new System.Drawing.Point(332, 60);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(137, 27);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Show/Hide";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(332, 104);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(137, 27);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Join";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(332, 162);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(137, 27);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "Go to Title";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(337, 202);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(131, 29);
+            this.button5.TabIndex = 12;
+            this.button5.Text = "AJAX";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.ClientSizeChanged += new System.EventHandler(this.button5_ClientSizeChanged);
+            this.button5.TextChanged += new System.EventHandler(this.AJAX);
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 450);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBoxFullName);
+            this.Controls.Add(this.textBoxLastName);
+            this.Controls.Add(this.textBoxFirstName);
+            this.Controls.Add(this.textBox1Title);
+            this.Controls.Add(this.FullName);
+            this.Controls.Add(this.LastName);
+            this.Controls.Add(this.FirstName);
+            this.Controls.Add(this.Title);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Title;
+        private System.Windows.Forms.Label FirstName;
+        private System.Windows.Forms.Label LastName;
+        private System.Windows.Forms.Label FullName;
+        private System.Windows.Forms.TextBox textBox1Title;
+        private System.Windows.Forms.TextBox textBoxFirstName;
+        private System.Windows.Forms.TextBox textBoxLastName;
+        private System.Windows.Forms.TextBox textBoxFullName;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ HomeWork3Exercise1/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HomeWork3Exercise1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FirstName_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Text = textBox1Title.Text;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBoxFullName.Text = textBoxFirstName.Text +" "+ textBoxLastName.Text;
+           // textBoxFirstName.AppendText(textBoxFirstName.Text + " " + textBoxLastName.Text);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Title.Visible = !Title.Visible;
+            textBox1Title.Visible = !textBox1Title.Visible;
+            button1.Visible = !button1.Visible;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox1Title.Focus();
+        }
+
+        private void AJAX(object sender, EventArgs e)
+        {
+            textBoxFirstName.AppendText(textBoxFirstName.Text + " " + textBoxLastName.Text);
+
+        }
+
+        private void button5_ClientSizeChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxFirstName_TextChanged(object sender, EventArgs e)
+        {
+
+            textBoxFullName.Text = textBoxFirstName.Text + " " + textBoxLastName.Text;
+
+        }
+
+        private void textBoxLastName_TextChanged(object sender, EventArgs e)
+        {
+
+            textBoxFullName.Text = textBoxFirstName.Text + " " + textBoxLastName.Text;
+        }
+    }
+}
